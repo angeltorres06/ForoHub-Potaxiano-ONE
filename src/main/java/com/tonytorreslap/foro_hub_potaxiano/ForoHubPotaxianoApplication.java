92b052107c61408af3957d388b4db27856ed862e
spiny006 <angeljworld@hotmail.com>
2026-03-09 21:53:51 -0600

    Inicio de proyecto...

---
+++ src/main/java/com/tonytorreslap/foro_hub_potaxiano/ForoHubPotaxianoApplication.java
@@ -0,0 +1,13 @@
+package com.tonytorreslap.foro_hub_potaxiano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoHubPotaxianoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForoHubPotaxianoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tonytorreslap/foro_hub_potaxiano/ForoHubPotaxianoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tonytorreslap.foro_hub_potaxiano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoHubPotaxianoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
